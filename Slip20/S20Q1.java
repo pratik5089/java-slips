6331e9d5c2d557e7ce77ae23d495fac0afb2dd5b
pratik5089 <121663595+pratik5089@users.noreply.github.com>
2022-12-30 20:04:32 +0530

    Add files via upload

---
+++ Slip20/S20Q1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+import java .util.*;
+ 
+
+class GFG 
+{
+
+    public static void main (String[] args)
+
+    {
+
+        int a[] = {10, 12, 5};
+ 
+
+        Arrays.sort(a);
+ 
+
+        for (int i = 0; i < 3; i++) 
+
+            System.out.print( a[i] + " ");
+
+    }
+}
