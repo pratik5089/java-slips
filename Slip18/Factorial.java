2b6c3fa4505067d8f5c8e1a5c254f942959198da
pratik5089 <121663595+pratik5089@users.noreply.github.com>
2022-12-30 20:02:48 +0530

    Add files via upload

---
+++ Slip18/Factorial.java
@@ -0,0 +1,15 @@
+class Factorial
+{
+	public static void main(String [] args)
+	{
+	int fact=1;
+	int num=Integer.parseInt(args[0]);
+	for(int i=1;i<=num;i++)
+	{
+		fact=fact*i;
+	}
+	System.out.println("Factorial is"+fact);	
+		
+	}
+	
+}
